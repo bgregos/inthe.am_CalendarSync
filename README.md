dd7879220d223e86db1e6c20cb6398a072eb04bc
bgregos <bgregos@gmail.com>
2017-01-13 17:47:13 -0500

    Add files

---
+++ README.md
@@ -0,0 +1,28 @@
+Installation Instructions
+
+1. Download the zip file, extract to same folder.
+
+2. Download requests, pytz and google-api-python-client with pip. (sudo pip install requests pytz google-api-python-client)
+
+3. Configure the program. Open intheAMsync.conf with your favorite  text editor. I'll explain the fields:
+- inthe.am API key: Your key obtained from inthe.am settings. Just copy/paste it in.
+- calendar id: You need to make a new calendar to sync events into. In the calendar settings for this new calendar, there should be a field that looks like <random letters/numbers>@group.calendar.google.com. Copy this in.
+- time zone: this should be primary time zone you are in. Match this to the settings for your calendar as well. Example: America/New_York.
+show due on prior day: if you put in True, you dates will show up on as due on the day before they're due in taskwarrior. This makes sense to some workflows, and no sense whatsoever to others. Set this to False if you're not impressed.
+
+4. Lastly, before you can run the program, you need your own Google Calendar API credentials, since you're running this yourself. 
+	Go to console.developers.google.com. 
+	Left side, click Credentials.
+	Click the blue Create Credentials box.
+	Click OAuth Client ID.
+	Select type Other and name it InTheAmSync
+	Exit the screen letting you copy/paste your credentials. You should now be out of the popup wizard.
+	On the right side, click the download button in the row containing InTheAMSync. It's labeled "Download JSON"
+	Take this file you downloaded, rename it to "client_id.json" and put it in the same directory as the config file  and sync script.
+	
+
+5. Run program with ./intheAMsync.py. It will open your default browser and ask you to log into your google account. Your details will be saved on disk at ~/.credentials, so this process will only need to occur once. If you want to use this program on a headless server, do this step on a non-headless box and and move the .credentials folder to your server, along with the program directory. The google API does not officially support headless operation, but this workaround works fine.
+
+6. Run the program every time you want to sync. You can do this automatically at set intervals with cron, as well. I'm sure the author of inthe.am would prefer you don't set the sync interval too high, however. 
+
+Other info regarding sync frequency: For Google Calendar, there's a maximum of 1 million API calls/day. Each sync uses at least one API call, and an additional one for every event added or deleted. At this point, moves count as two calls. If you have really huge task lists or use one Google API key for lots of people, you may run into the the limit there. 
---
+++ intheAMsync.py
@@ -0,0 +1,253 @@
+#!/usr/bin/python3
+"""This program syncs in.the.am to Google Calendar"""
+from __future__ import print_function
+from configparser import ConfigParser
+from datetime import datetime
+
+import calendar
+import json
+import os
+import requests
+
+import httplib2
+import pytz
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    FLAGS = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    FLAGS = None
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if FLAGS:
+            flags=tools.argparser.parse_args(args=[])
+            credentials = tools.run_flow(flow, store, FLAGS)
+        #else: # Needed only for compatibility with Python 2.6
+        #    credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/calendar-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+CLIENT_SECRET_FILE = 'client_id.json'
+APPLICATION_NAME = 'InTheAmSync'
+credentials = get_credentials()
+http = credentials.authorize(httplib2.Http())
+SERVICE = discovery.build('calendar', 'v3', http=http)
+CAL_ID = '' #Imported at start from config file
+
+def get_tasks(apikey):
+    """Returns a list of tasks"""
+    # Replace with the correct URL
+    url = 'https://inthe.am/api/v2/tasks/'
+
+    my_response = requests.get(url, headers={'Authorization': 'Token '+apikey})
+
+    print('HTTP '+str(my_response.status_code))
+    return my_response.text
+
+def parse_tasks(key):
+    """Returns a dictionary with names and due dates of tasks"""
+    data = json.loads(get_tasks(key))
+    #print(data[0]["description"])
+    print('Total tasks: '+str(len(data)))
+    tasklist = {}
+    for i, j in enumerate(data):
+        if "due" in j:
+            print(str(i)+': '+j['description'])
+            print(str(i)+': '+j['due'])
+            tasklist[j['description']] = j['due']
+        else:
+            print('Skipping task with no due date.')
+    return tasklist
+
+def main():
+    config = ConfigParser()
+    config.read('intheAMsync.conf')
+    api_key = config.get('Settings', 'inthe.am API key')
+    global CAL_ID
+    CAL_ID = config.get('Settings', 'calendar id')
+    timezone = config.get('Settings', 'time zone')
+    prior_day = config.getboolean('Settings', 'show due on prior day')
+    tasklist = parse_tasks(api_key)
+    print('Tasks recieved from inthe.am. Uploading to Calendar...')
+    page_token = None
+    callist = SERVICE.events().list(
+        calendarId=CAL_ID,
+        pageToken=page_token,
+        timeZone='Europe/London').execute()
+
+    for desc, time in tasklist.items(): #Prevents duplicating events
+        nameflag = False #becomes true if another event shares the name
+        dueflag = False
+        date = fix_date(time, timezone)
+        if prior_day:
+            date = decrement_day(date)
+        for event in callist['items']:
+            if desc == event['summary']:
+                nameflag = True
+            if date == event['start']['date']:
+                dueflag = True
+        if not (nameflag is True and dueflag is True):
+            create_event(desc, date)
+            print('Uploaded event')
+        else:
+            print('Duplicate event detected and not uploaded.')
+
+    for event in callist['items']: #deletes removed events
+        hit = True
+        for desc, time in tasklist.items():
+            due = fix_date(time, timezone)
+            if prior_day:
+                due = decrement_day(due)
+            nameflag = False
+            dueflag = False
+            if desc == event['summary']:
+                nameflag = True
+            if due == event['start']['date']:
+                dueflag = True
+            if nameflag is True and dueflag is True:
+                hit = False #If name and due date matches, do not delete
+        if hit:
+            SERVICE.events().delete( #delete event
+                calendarId=CAL_ID,
+                eventId=event['id']).execute()
+            print('Purged deleted event.')
+
+    print('Upload to Calendar complete!')
+
+def create_event(description, date):
+    """Creates a Google Calendar event"""
+    print('Creating event with start date: ' + date)
+    event = {
+        "start": {
+            "date": date
+        },
+        "end": {
+            "date": increment_day(date)
+        },
+        "creator": {
+            "self": "true"
+        },
+        "summary": description
+    }
+    event = SERVICE.events().insert(calendarId=CAL_ID, body=event).execute()
+
+def fix_date(time, timezone):
+    """
+    This turns what inTheAM returns as the due time into a date adjusted
+    for time zones, daylight savings, etc.
+    Output Format: YYYY-MM-DD
+    """
+    date = time.split('T')[0]
+    time = time.split('T')[1]
+    due = date + 'T' + time + "UTC"
+    #Example due: 2017-01-14T04:44:00ZUTC
+    time_format = '%Y-%m-%dT%H:%M:%SZ%Z'
+    dtime = datetime.strptime(due, time_format)
+    dtime = pytz.utc.localize(dtime)
+    converted_time = dtime.astimezone(pytz.timezone(timezone))
+    start_date = converted_time.isoformat().split(' ')[0]
+    start_date = start_date.split('T')[0] #the two splits get us only the date
+    return start_date
+
+def decrement_day(time):
+    """
+    Takes input in YYYY-MM-DD format, and decrements it by one day, respecting month
+    and year changes, along with leap year.
+    """
+    date = time.split('-')
+    date[2] = str(int(date[2]) - 1).zfill(2)
+    if date[2] == '0':
+        if date[1] == '1':
+            date[0] = str(int(date[0])-1).zfill(4)
+            date[1] = '12'
+            date[2] = '31'
+        else:
+            if int(date[1])-1 in {9, 4, 6, 11}:
+                date[2] = '30'
+                date[1] = str(int(date[1])-1).zfill(2)
+            elif int(date[1])-1 in {1, 3, 5, 7, 8, 10}:
+                date[2] = '31'
+                date[1] = str(int(date[1])-1).zfill(2)
+            elif int(date[1])-1 == 2:
+                date[1] = '1'
+                if calendar.isleap:
+                    date[2] = '29'
+                else:
+                    date[2] = '28'
+
+    #if none of the above statements ran, the day simply got decremented.
+    return date[0]+'-'+date[1]+'-'+date[2]
+
+def increment_day(day):
+    """Takes a YYYY-MM-DD formatted string and increments it by one day, keeping
+    in mind months, years, leap years, etc."""
+    date = day.split("-")
+    date[2] = str(int(date[2]) + 1) #increment day
+    #perform normal month checks
+    if ((date[2] == '31' and date[1] in {4, 6, 9, 11}) or
+            (date[2] == 32 and date[1] in {1, 3, 5, 7, 8, 10, 12})):
+        date[2] = '1'
+        date[1] = str(int(date[1])+1)
+    #Roll over year if neccessary
+    if date[1] == '13':
+        date[1] = '1'
+        date[0] = str(int(date[0])+1)
+    #Handle February, including leap year
+    if ((calendar.isleap and date[1] == '2' and date[2] == '29') or
+            (not calendar.isleap and date[1] == '2' and date[2] == '28')):
+        date[1] = '3'
+        date[2] = '1'
+    #return incremented date
+    return date[0]+'-'+date[1]+'-'+date[2]
+
+
+def get_service():
+    """Gets the Google Calendar API service.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+    return service
+
+def list_calendars():
+    page_token = None
+    calendar_list = SERVICE.calendarList().list(pageToken=page_token).execute()
+    i = 1
+    for calendar_list_entry in calendar_list['items']:
+        print('Title for '+str(i)+': '+calendar_list_entry['summary'])
+        print(calendar_list_entry['id'])
+        i = i + 1
+
+
+if __name__ == '__main__':
+    #list_calendars()
+    main()
+
